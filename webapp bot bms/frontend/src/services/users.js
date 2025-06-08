0271424d18ad44c2854ab51d383bab5483d48151
amilcargonzalo98 <134455884+amilcargonzalo98@users.noreply.github.com>
2025-06-07 22:01:40 -0300

    Add user deletion with confirmation

--- webapp bot bms/frontend/src/services/users.js
+++ webapp bot bms/frontend/src/services/users.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 
 export const fetchUsers = () => axios.get('/api/users');
 export const createUser = (user) => axios.post('/api/users', user);
+export const deleteUser = (id) => axios.delete(`/api/users/${id}`);
